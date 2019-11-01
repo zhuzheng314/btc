67a5fd8db011c63cbe278013ed553935cb60d0a9
zhuzheng <zhuzheng@startdt.net>
2019-11-01 23:24:54 +0800

    api

---
+++ src/utils/config.js
@@ -0,0 +1,13 @@
+const key = 'f3d985bc-887d-45bb-8db4-ceff2sedd87fe'
+const secret = '1319B1CBE5ddddwwsEF56DAA7'
+const passphrase = 'xewwdd'
+const urlHost = 'https://www.okex.com'
+const websocekHost = 'wss://real.okex.com:10442/ws/v3'
+
+export {
+  key,
+  secret,
+  passphrase,
+  urlHost,
+  websocekHost
+}
